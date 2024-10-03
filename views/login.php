82bb91423d4cea5786c622ee9bdfd192b2844536
Renzzo131 <renzoeduardo1304@gmail.com>
2024-10-03 08:37:00 -0500

    cambios nuevos
    Algunas correciones

--- views/login.php
+++ views/login.php
@@ -1,1 +1,22 @@
 <p>Login</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        input{
+            margin: ;
+        }
+    </style>
+</head>
+<body>
+    <form action="">
+        <input type="text" placeholder="Nombre">
+        <input type="email" placeholder="Correo electrónico">
+        <input type="password" placeholder="Contraseña">
+        <button type="submit">Enviar al chistoso</button>
+    </form>
+</body>
+</html>
